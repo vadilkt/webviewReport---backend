2085cf60fe72ccae4df7ed97386162cde93a857b
Kwekam Vadil <vadilkwekam@gmail.com>
2024-09-14 15:03:48 +0100

    tout ok

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO transmission_sheet (destination, provenance, date_emission, num_bordereau, nbre_courrier)
+VALUES ('City A', 'City B', '2023-01-01', 'B12345', 5);
+
+
+INSERT INTO courier (courier_number, courrier_date, dossier_number, transmission_sheet_id, expediteur, num_courrier, objet)
+VALUES ('C123', '2023-01-02', 'D123', 1, 'Expéditeur 1', 'C001', 'Document');
+
+
+INSERT INTO attachment (file_name, courier_id)
+VALUES ('document1.pdf', 1);
+
