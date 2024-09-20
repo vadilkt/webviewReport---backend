b2fd4d797399749cda54b6258e55a13e5e07eb2a
Kwekam Vadil <vadilkwekam@gmail.com>
2024-09-20 01:34:28 +0100

    version ok

--- src/main/java/com/example/web_report_viewer/service/CourrierService.java
+++ src/main/java/com/example/web_report_viewer/service/CourrierService.java
@@ -7,16 +7,20 @@ import com.example.web_report_viewer.repository.TransmissionSheetRepository;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
 import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.engine.fill.JasperReportSource;
 import net.sf.jasperreports.export.SimpleDocxReportConfiguration;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import net.sf.jasperreports.engine.*;
 import org.springframework.util.ResourceUtils;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Service
@@ -28,7 +32,11 @@ public class CourrierService {
     @Autowired
     private TransmissionSheetRepository transmissionSheetRepository;
 
-    public byte[] exportReport(String reportFormat, Long sheetId) throws FileNotFoundException, JRException {
+    public byte[] exportReport(String reportFormat, Long sheetId) throws IOException, JRException {
+        String userHome = System.getProperty("user.home");
+        String desktopPath = userHome +"/Desktop/";
+        String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String fileName;
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         Optional<TransmissionSheet> optionalTransmissionSheet = transmissionSheetRepository.findById(sheetId);
 
@@ -54,38 +62,67 @@ public class CourrierService {
 
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, couriersDataSource);
 
-        if (jasperPrint != null) {
-            System.out.println("Le rapport a été généré avec succès.");// Enregistrez ou affichez le rapport
-            JasperExportManager.exportReportToPdfFile(jasperPrint, "C://Users//Vadil Kwekam/rapport5.pdf");
-        } else {
-            System.out.println("Erreur : Le rapport n'a pas été généré.");
-        }
+//        if (jasperPrint != null) {
+//            System.out.println("Le rapport a été généré avec succès.");// Enregistrez ou affichez le rapport
+//            JasperExportManager.exportReportToPdfFile(jasperPrint, "C://Users//Vadil Kwekam/rapport9.pdf");
+//        } else {
+//            System.out.println("Erreur : Le rapport n'a pas été généré.");
+//        }
 
         switch (reportFormat.toLowerCase()) {
             case "pdf":
+                fileName = "bordereau_" + timestamp + ".pdf";
+                String pdfPath = desktopPath + fileName;
+                JasperExportManager.exportReportToPdfFile(jasperPrint, pdfPath);
+                System.out.println("Fichier exporté à : " + pdfPath);
                 return JasperExportManager.exportReportToPdf(jasperPrint);
+
+            case "html":
+                fileName = "bordereau_" + timestamp + ".html";
+                String htmlPath = desktopPath + fileName;
+                JasperExportManager.exportReportToHtmlFile(jasperPrint, htmlPath);
+                System.out.println("Fichier exporté à : " + htmlPath);
+                break;
+
+            case "xml":
+                fileName = "bordereau_" + timestamp + ".xml";
+                String xmlPath = desktopPath + fileName;
+                JasperExportManager.exportReportToXmlFile(jasperPrint, xmlPath, true);
+                System.out.println("Fichier exporté à : " + xmlPath);
+                break;
+
             case "excel":
-                JRXlsExporter xlsExporter = new JRXlsExporter();
-                xlsExporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-                xlsExporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
+                fileName = "bordereau_" + timestamp + ".xlsx";
+                String excelPath = desktopPath + fileName;
+                JRXlsxExporter xlsxExporter = new JRXlsxExporter();
+                xlsxExporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+                xlsxExporter.setExporterOutput(new SimpleOutputStreamExporterOutput(excelPath));
                 SimpleXlsxReportConfiguration xlsxConfiguration = new SimpleXlsxReportConfiguration();
                 xlsxConfiguration.setDetectCellType(true);
                 xlsxConfiguration.setOnePagePerSheet(false);
                 xlsxConfiguration.setRemoveEmptySpaceBetweenRows(true);
-                xlsExporter.setConfiguration(xlsxConfiguration);
-                xlsExporter.exportReport();
-                return outputStream.toByteArray();
+                xlsxExporter.setConfiguration(xlsxConfiguration);
+                xlsxExporter.exportReport();
+                System.out.println("Fichier exporté à : " + excelPath);
+                break;
+
             case "word":
+                fileName = "bordereau_" + timestamp + ".docx";
+                String wordPath = desktopPath + fileName;
                 JRDocxExporter docxExporter = new JRDocxExporter();
                 docxExporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-                docxExporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
+                docxExporter.setExporterOutput(new SimpleOutputStreamExporterOutput(wordPath));
                 SimpleDocxReportConfiguration docxConfiguration = new SimpleDocxReportConfiguration();
                 docxConfiguration.setFlexibleRowHeight(true);
                 docxExporter.setConfiguration(docxConfiguration);
                 docxExporter.exportReport();
-                return outputStream.toByteArray();
+                System.out.println("Fichier exporté à : " + wordPath);
+                break;
+
             default:
                 throw new IllegalArgumentException("Format de rapport non supporté: " + reportFormat);
         }
+
+        return outputStream.toByteArray();
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,11 @@
 
+
+
 INSERT INTO transmission_sheet (destination, provenance, date_emission, num_bordereau, nbre_courrier)
 VALUES ('City A', 'City B', '2023-01-01', 'B12345', 5);
 
 
-INSERT INTO courier (courier_number, courrier_date, dossier_number, transmission_sheet_id, expediteur, num_courrier, objet)
+INSERT INTO courier (courrier_date, dossier_number, transmission_sheet_id, expediteur, num_courrier, objet)
 VALUES ('C123', '2023-01-02', 'D123', 1, 'Expéditeur 1', 'C001', 'Document');
 
 
